d0a1b8898c28d4f6e766bf61ce19a29f7345f721
rajanChavda-Torinit <rajan.chavda@torinit.com>
2024-10-22 12:02:12 +0530

    changes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import PdfViewerComponent from './components/PdfViewerComponent';
-import DragAndDropSignature from './components/DragAndDropSignature';
+// import DragAndDropSignature from './components/DragAndDropSignature';
 
 const App: React.FC = () => {
 
   return (
     <div className="App" style={{ width: "100vw" }}>
       <div className="PDF-viewer">
-        {/* <PdfViewerComponent document={"document.pdf"} /> */}
-        <DragAndDropSignature document={"document.pdf"}/>
+        <PdfViewerComponent document={"document.pdf"} />
+        {/* <DragAndDropSignature document={"document.pdf"}/> */}
       </div>
     </div>
   );
